33851e1b78f76622bab3d963558fe721ec85377b
ferdianafsa <farisafza201002@gmail.com>
2021-03-17 11:07:10 +0700

    fixed views homes

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,7 @@ class HomesController < ApplicationController
 before_action :authenticate_user!
     def index
       @list_lelang = Lelang.where(status: true).order(tanggal_lelang: :desc).page params[:page]
+      @lelang_terakhir = Lelang.last
       @list_sold_lelang = Lelang.where(status: false).order(tanggal_lelang: :desc).page params[:page]
     end
 
